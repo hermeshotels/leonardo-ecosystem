ea0fe7f37d45375ef9cea748b8b18628902b6509
Wanny Miarelli <wannymiarelli@live.com>
2017-03-07 20:18:11 +0100

    added env variables

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,10 +5,12 @@ module.exports = {
 			script: '/home/ubuntu/code/bol/server.js',
 			watch: true,
 			env: {
-				NODE_ENV: 'development'
+				NODE_ENV: 'development',
+				APIURL: 'http://34.251.162.93:3000/api'
 			},
 			env_production: {
-				NODE_ENV: 'production'
+				NODE_ENV: 'production',
+				APIURL: 'http://34.251.162.93:3000/api'
 			}
 		},
 		{
